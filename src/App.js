dc7ac8e4106abb2faef0e922350a8aac9e3f2c3d
nadzeya molchan <nadzeya.molchan@itechart-group.com>
2021-06-11 11:50:56 +0300

    add header component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 import './App.css';
 
 function App() {
-  return (
-    <div>
-            <Route exact path='/' component={Homepage}/>
-            <Route path='/shop' component={ShopPage}/>
-
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+                <Switch>
+                    <Route exact path='/' component={Homepage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                </Switch>
+        </div>
+    );
 }
 
 export default App;
