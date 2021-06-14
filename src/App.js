841d609304920ba8485e0c6a9c0311604c54b9dd
nadzeya molchan <nadzeya.molchan@itechart-group.com>
2021-06-14 12:05:58 +0300

    integrated sign-in firebase into application, refactor sign-in form style

--- src/App.js
+++ src/App.js
@@ -5,20 +5,42 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import Header from "./components/header/header.component";
+import {auth} from './firebase/firebase.utils';
 
 import './App.css';
 
-function App() {
-    return (
-        <div>
-            <Header/>
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+            this.setState({currentUser: user});
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser}/>
                 <Switch>
                     <Route exact path='/' component={Homepage}/>
                     <Route path='/shop' component={ShopPage}/>
                     <Route path='/signin' component={SignInAndSignUp}/>
                 </Switch>
-        </div>
-    );
+            </div>
+        )
+    }
 }
 
 export default App;
