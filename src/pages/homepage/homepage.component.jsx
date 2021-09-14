70b6cb40d16596a9870c7c0e9847de27d50b5114
nadzeya molchan <nadzeya.molchan@itechart-group.com>
2021-09-14 19:32:32 +0300

    added styled components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import './homepage.styles.scss';
+import {HomePageContainer} from "./homepage.styles";
 import Directory from '../../components/directory/directory';
 
 
 const Homepage = () => {
-    return(<div className="homepage">
+    return (<HomePageContainer>
         <Directory/>
-    </div>)
+    </HomePageContainer>)
 }
 
 export default Homepage;
