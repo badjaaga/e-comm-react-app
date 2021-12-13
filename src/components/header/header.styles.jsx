3ff446d115c494ca3ee4e2b21313c04a530a4195
Nadzeya Molchan <nadzeya_molchan@epam.com>
2021-12-13 19:58:31 +0300

    styled components

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,11 +1,6 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 
-/*const OptionContainerStyles = css`
-  padding: 10px 15px;
-  cursor: pointer;
-`;*/
-
 export const HeaderContainer = styled.div`
   height: 70px;
   width: 100%;
