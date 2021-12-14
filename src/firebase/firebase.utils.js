83d7462deac9a44abec29640b4687551b354d717
Nadzeya Molchan <nadzeya_molchan@epam.com>
2021-12-14 14:54:09 +0300

    update redux to store shop data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,43 +3,58 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCBqtfkU8e8fzSF4A5eViq6AL7XpgBbK_E",
-    authDomain: "crown-db-55f68.firebaseapp.com",
-    projectId: "crown-db-55f68",
-    storageBucket: "crown-db-55f68.appspot.com",
-    messagingSenderId: "901588512790",
-    appId: "1:901588512790:web:1443ecef0dd8b526f70537"
+  apiKey: "AIzaSyCBqtfkU8e8fzSF4A5eViq6AL7XpgBbK_E",
+  authDomain: "crown-db-55f68.firebaseapp.com",
+  projectId: "crown-db-55f68",
+  storageBucket: "crown-db-55f68.appspot.com",
+  messagingSenderId: "901588512790",
+  appId: "1:901588512790:web:1443ecef0dd8b526f70537"
 };
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+  if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = await userRef.get();
+  const snapShot = await userRef.get();
 
-    if(!snapShot.exists){
-        const {displayName, email} = userAuth;
-        const createdAt = new Date();
+  if (!snapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
 
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            })
-        } catch (err){
-            console.log('error creating user', err.message);
-        }
-
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (err) {
+      console.log('error creating user', err.message);
     }
-    return userRef;
+
+  }
+  return userRef;
 }
 
+export const convertCollectionsSnapshotToMap = (collectionsSnapshot) => {
+  const transformedToCollection = collectionsSnapshot.docs.map(doc => {
+    const {title, items} = doc.data();
 
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      items
+    }
+  });
+
+  return transformedToCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+      },{})
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
